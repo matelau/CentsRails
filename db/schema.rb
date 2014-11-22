b11fc2c470b31cd81a3f4334e6569739a47e697b
oakfield <michael.christian.smith@gmail.com>
2014-11-21 22:42:38 -0700

    added user tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141122041631) do
+ActiveRecord::Schema.define(version: 20141122051702) do
 
   create_table "colis", force: true do |t|
     t.float  "cost_of_living", limit: 24
@@ -21,6 +21,52 @@ ActiveRecord::Schema.define(version: 20141122041631) do
     t.float  "health_care",    limit: 24
     t.float  "utilities",      limit: 24
     t.string "location"
+  end
+
+  create_table "completeds", force: true do |t|
+    t.integer  "user_id"
+    t.string   "section"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "queries", force: true do |t|
+    t.integer  "user_id"
+    t.string   "url"
+    t.integer  "query"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "scalars", force: true do |t|
+    t.integer  "user_id"
+    t.float    "misc",           limit: 24
+    t.float    "housing",        limit: 24
+    t.float    "books",          limit: 24
+    t.float    "insurance",      limit: 24
+    t.float    "utilities",      limit: 24
+    t.string   "type"
+    t.string   "name"
+    t.float    "food",           limit: 24
+    t.float    "savings",        limit: 24
+    t.float    "loans",          limit: 24
+    t.float    "tuition",        limit: 24
+    t.float    "healthcare",     limit: 24
+    t.float    "debt",           limit: 24
+    t.float    "transportation", limit: 24
+    t.boolean  "public"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.float    "debt",       limit: 24
+    t.float    "books",      limit: 24
+    t.float    "savings",    limit: 24
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "weather_records", force: true do |t|
