8e10814b0b53d1dd15249d3c1e4c372a845b019d
Wesley Gormley <wesleypalmer83@gmail.com>
2014-11-23 22:26:46 -0700

    user can now skip steps in the wizard if they already know some school information. back links are still broken, just going back to the last page no matter what

--- app/controllers/wizard_controller.rb
+++ app/controllers/wizard_controller.rb
@@ -1,15 +1,31 @@
 class WizardController < ApplicationController
 	def start
+		cookies[:skip] = "false"
 	end
 	def education
+		cookies[:skip] = "false"
 	end
 	def career
+		# if cookies[:skip] = true
+		# 	@prev_link = "/wizard/major"
+		# 	@prev_name = "(major)"
+		# else
+		# 	@prev_link = "/wizard/school"
+		# 	@prev_name = "(school)"
+		# end
 	end
 	def city
 	end
 	def school
 	end
 	def major
+		if cookies[:skip] == "true"
+			@next_link = "/wizard/career"
+			@next_name = "(career)"
+		else
+			@next_link = "/wizard/school"
+			@next_name = "(school)"
+		end
 	end
 
 end
