d17cd24cba6765cebdddee9a0fe61433a43c536a
oakfield <michael.christian.smith@gmail.com>
2014-12-03 23:34:07 -0700

    added comments for Austin

--- app/controllers/api/v1/coli_controller.rb
+++ app/controllers/api/v1/coli_controller.rb
@@ -1,13 +1,19 @@
 class Api::V1::ColiController < ApplicationController
-	def index
-		if params[:id]  	
-			render json: Coli.find(params[:id]), status: 200
-		else
-			render json: 'Enter an ID.'
-    end
-	end
+### Just for testing
+#	def index
+#		if params[:id]  	
+#			render json: Coli.find(params[:id]), status: 200
+#		else
+#			render json: 'Enter an ID.'
+#		end
+#	end
 
-  def show
+	# Retrieve all location data by location name.
+	# (Well, it SHOULD. Right now it looks up some location data by ID.)
+	# TODO:	(1) Accept JSON data using the correct protocol.
+	# 			(2) Retrieve weather data also.
+	#				(3) Error checking, including reasonable status codes for bad input.
+	def show
 		render json: Coli.find(params[:id]), status: 200
-  end
+	end
 end
