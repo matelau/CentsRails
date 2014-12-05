2ab453e843446c5685a65322175d7a7ad3f5fed9
Wesley Gormley <wesleypalmer83@gmail.com>
2014-12-04 23:03:59 -0700

    add cli viz and interactivity, probably has some bugs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@
 //= require ripples
 //= require_tree .
 
-var active_tab, type, data, hide_1, hide_2;
+var active_tab, type, data, hide_1, hide_2, axis_location, horz_locs, min, max, above_1, above_2, below_1, below_2;
 
 
 var sketch = new Processing.Sketch();
@@ -38,6 +38,7 @@ function sketchProc(processing) {
 		active_tab = 1;
 		hide_1 = false;
 		hide_2 = false;
+		horz_locs = [87, 145, 215, 308, 389, 488, 577];
 		
 
 		//load font
@@ -45,6 +46,10 @@ function sketchProc(processing) {
 
 		//var to hold all data relevant to a given category
 		data = new Array();
+		above_1 = new Array();
+		above_2 = new Array();
+		below_1 = new Array();
+		below_2 = new Array();
 		//create dummy data
 
 		//weather
@@ -97,14 +102,13 @@ function sketchProc(processing) {
 		data["weatherlow_2_11"] = 53.5;
 		data["weatherlow_1_12"] = 27.0;
 		data["weatherlow_2_12"] = 45.5;
-		//this should be scaled up and down a little, +- 10% of range
 		data["max_weather_1"] = 95.5;
 		data["min_weather_1"] = 26.0;
 		data["max_weather_2"] = 106.5;
 		data["min_weather_2"] = 45.5;
 
-		data["name1"] = "Salt Lake City, UT";
-		data["name2"] = "Phoenix, AZ";
+		data["location_1"] = "Salt Lake City, UT";
+		data["location_2"] = "Phoenix, AZ";
 
 		//labor statstics
 		data["unemploy_1"] = 3.4;
@@ -114,13 +118,44 @@ function sketchProc(processing) {
 		data["econgrow_1"] = 4.4;
 		data["econgrow_2"] = 3.3;
 
+		//cost of living dummy data
+		// data["cli_1_1"] = 101;
+		// data["cli_1_2"] = 99;
+		// data["cli_2_1"] = 102;
+		// data["cli_2_2"] = 98;
+		// data["cli_3_1"] = 103;
+		// data["cli_3_2"] = 97;
+		// data["cli_4_1"] = 105;
+		// data["cli_4_2"] = 96;
+		// data["cli_5_1"] = 110;
+		// data["cli_5_2"] = 95;
+		// data["cli_6_1"] = 115;
+		// data["cli_6_2"] = 93;
+		// data["cli_7_1"] = 120;
+		// data["cli_7_2"] = 92;
+		data["cli_1_1"] = 102;
+		data["cli_1_2"] = 96;
+		data["cli_2_1"] = 94;
+		data["cli_2_2"] = 92;
+		data["cli_3_1"] = 95;
+		data["cli_3_2"] = 100;
+		data["cli_4_1"] = 95;
+		data["cli_4_2"] = 106;
+		data["cli_5_1"] = 119;
+		data["cli_5_2"] = 97;
+		data["cli_6_1"] = 105;
+		data["cli_6_2"] = 101;
+		data["cli_7_1"] = 92;
+		data["cli_7_2"] = 99;
+		data["max_cli_1"] = 119;
+		data["min_cli_1"] = 92;
+		data["max_cli_2"] = 106;
+		data["min_cli_2"] = 92;
 
 
 
-
-
-		document.getElementById("search_1_name").value = data["name1"];
-		document.getElementById("search_2_name").value = data["name2"];
+		document.getElementById("search_1_name").value = data["location_1"];
+		document.getElementById("search_2_name").value = data["location_2"];
 
 
 
@@ -135,6 +170,102 @@ function sketchProc(processing) {
 
 		if (type == "city")
 			draw_city();
+
+		//show cli details
+		if (type == "city" && active_tab == 1)
+		{
+			var category = ["Overall costs in", "Goods in", "Groceries in", "Health care costs in", "Housing costs in", "Transportation costs in", "Utilities in"];
+			for (var i=0; i<horz_locs.length; i++)
+			{
+				//aligned horizontally
+				if (processing.mouseX < horz_locs[i] && processing.mouseX >= (horz_locs[i] - 16) && !hide_1)
+				{
+						//location 1, postive change
+						var data1 = data["cli_" + String(i+1) + "_1"];
+						if (processing.mouseY < (axis_location-10) && processing.mouseY > above_1[i])
+						{
+							processing.fill(255);
+							processing.stroke(0);
+							var length = processing.max(data["location_1"].length, category[i]);
+							var height;
+							if (processing.mouseY < 60)
+								height = 62;
+							else
+								height = processing.mouseY;
+							processing.rect(processing.mouseX-(120+length)/2, height-60, length+120, 52);
+							processing.fill(0);
+							processing.textAlign(processing.CENTER);
+							processing.text(category[i], processing.mouseX, height-48);
+							processing.text(data["location_1"], processing.mouseX, height-36);
+							processing.text("are " + String(data1 - 100) + "% above the", processing.mouseX, height-24);
+							processing.text("national average.", processing.mouseX, height-12);
+
+						}
+						if (processing.mouseY > (axis_location+10) && processing.mouseY < below_1[i])
+						{
+							processing.fill(255);
+							processing.stroke(0);
+							var length = processing.max(data["location_1"].length, category[i]);
+							var height;
+							if (processing.mouseY > 595)
+								height = 593;
+							else
+								height = processing.mouseY;
+							processing.rect(processing.mouseX-(120+length)/2, height-60, length+120, 52);
+							processing.fill(0);
+							processing.textAlign(processing.CENTER);
+							processing.text(category[i], processing.mouseX, height-48);
+							processing.text(data["location_1"], processing.mouseX, height-36);
+							processing.text("are " + String(100 - data1) + "% below the", processing.mouseX, height-24);
+							processing.text("national average.", processing.mouseX, height-12);
+
+						}
+						
+				}
+				if (processing.mouseX >= horz_locs[i] && processing.mouseX < (horz_locs[i] + 16) && !hide_2)
+				{
+					var data2 = data["cli_" + String(i+1) + "_2"];
+					if (processing.mouseY < (axis_location-10) && processing.mouseY > above_2[i])
+					{
+						processing.fill(255);
+						processing.stroke(0);
+						var length = processing.max(data["location_2"].length, category[i]);
+						var height;
+						if (processing.mouseY < 60)
+							height = 62;
+						else
+							height = processing.mouseY;
+						processing.rect(processing.mouseX-(120+length)/2, height-60, length+120, 52);
+						processing.fill(0);
+						processing.textAlign(processing.CENTER);
+						processing.text(category[i], processing.mouseX, height-48);
+						processing.text(data["location_2"], processing.mouseX, height-36);
+						processing.text("are " + String(data2 - 100) + "% above the", processing.mouseX, height-24);
+						processing.text("national average.", processing.mouseX, height-12);
+
+					}
+					if (processing.mouseY > (axis_location+10) && processing.mouseY < below_2[i])
+					{
+						processing.fill(255);
+						processing.stroke(0);
+						var length = processing.max(data["location_2"].length, category[i]);
+						var height;
+						if (processing.mouseY > 595)
+							height = 593;
+						else
+							height = processing.mouseY;
+						processing.rect(processing.mouseX-(120+length)/2, height-60, length+120, 52);
+						processing.fill(0);
+						processing.textAlign(processing.CENTER);
+						processing.text(category[i], processing.mouseX, height-48);
+						processing.text(data["location_2"], processing.mouseX, height-36);
+						processing.text("are " + String(100 - data2) + "% below the", processing.mouseX, height-24);
+						processing.text("national average.", processing.mouseX, height-12);
+
+					}
+				}
+			}
+		}
 
 	};
 
@@ -151,19 +282,168 @@ function sketchProc(processing) {
 	};
 
 	function cost_of_living() {
-		//draw axis
+
+		//need to check for hugh outliers that will skew viz and make data impossible to read
+
+
+		//calculate min and max depending on what data is being shown/hidden
+		
+		if (!hide_2 && hide_1)
+		{
+			min = data["min_cli_2"];
+			max = data["max_cli_2"];
+
+		}
+		else if (hide_2 && !hide_1)
+		{
+			min = data["min_cli_1"];
+			max = data["max_cli_1"];
+		}
+		else
+		{
+			min = processing.min(data["min_cli_1"], data["min_cli_2"]);
+			max = processing.max(data["max_cli_1"], data["max_cli_2"]);
+			//min = min - ((max-min));
+			//max = max + ((max-min));
+		}
+
+
+
+		//draw axis depending on where national average (100) falls within the calculated min and max
+		
+		axis_location;
+		if (max <= 100)
+			axis_location = 60;
+		else if (min > 100)
+			axis_location = 315;
+		else
+			axis_location = 60 + 255 * (1 - ((100 - min) / (max - min)));
+
+
+		//draw scale
+		processing.stroke(235);
 		processing.fill(0);
-		processing.stroke(0);
+		processing.textAlign(processing.RIGHT);
+		var step = (max-min)/11;
+		//var range = max-min;
+		for (var i=max; i>100; i-=step)
+		{
+				processing.line(60, 45 + (axis_location-15-45)*(1-(i-100)/(max-100)), 605, 45 + (axis_location-15-45)*(1-(i-100)/(max-100)));
+				processing.text(processing.round(i) + "%", 55, 50 + (axis_location-15-45)*(1-(i-100)/(max-100)));
+		}
+		var step = (max-min)/10;
+		for (var i=min; i<100; i+=step)
+		{
+			processing.line(60, 330 - (330-(axis_location+15))*(1-(100-i)/(100-min)), 605, 330 - (330-(axis_location+15))*(1-(100-i)/(100-min)));
+			processing.text(processing.round(i) + "%", 55, 335 - (330-(axis_location+15))*(1-(100-i)/(100-min)));
+		}
+
+		// var max_percent = (max-100)/(max-min)+0.049;
+		// var min_percent = (100-min)/(max-min)+0.049;
+		// for (var i=0.1; i<max_percent-0.1; i+=0.1)
+		// {
+		// 	processing.text(processing.round(i*(max-min)) + "%", 55, (axis_location-15) - (axis_location-15+60)*i+5);
+		// 	processing.line(60, (axis_location-15) - (axis_location-15+60)*i, 605, (axis_location-15) - (axis_location-15+60)*i);
+
+		// }
+		// for (var i=0.1; i<min_percent-0.1; i+=0.1)
+		// {
+		// 	processing.text(processing.round("-" + i*(max-min)) + "%", 55, (axis_location+15) + (axis_location+15+60)*i+5);
+		// 	processing.line(60, (axis_location+15) + (axis_location+15+60)*i, 605, (axis_location+15) + (axis_location+15+60)*i);
+		// }
+
+		
+
+		//draw a rectangle to highlight better or worse than national average 
+		processing.fill(245);
+		processing.noStroke();
+		// if (axis_location != 50 || axis_location != 325)
+		// 	processing.rect(60, 50, 545, 275 * (1 - ((100 - min) / (max - min))) - 15);
+
+		processing.fill(255);
 		processing.strokeWeight(2);
-		processing.line(50, 50, 50, 325);
-		processing.line(50, 187, 605, 187);
-		//add scale
+		processing.rect(60, axis_location - 9, 545, 19);
 
-		//add labels
+		
 
+		
+		processing.textAlign(processing.CENTER);
+		processing.fill(0);
+		processing.text("OVERALL", horz_locs[0], axis_location+5);
+		processing.text("GOODS", horz_locs[1], axis_location+5);
+		processing.text("GROCERIES", horz_locs[2], axis_location+5);
+		processing.text("HEALTH CARE", horz_locs[3], axis_location+5);
+		processing.text("HOUSING", horz_locs[4], axis_location+5);
+		processing.text("TRANSPORTATION", horz_locs[5], axis_location+5);
+		processing.text("UTILITIES", horz_locs[6], axis_location+5);
+		
 		//draw data
+		processing.noStroke();
+		for (var i=1; i<=7; i++)
+		{
+			if (!hide_1)
+			{
+				var data1 = data["cli_" + i + "_1"];
+				processing.fill(main);
+				if (data1 > 100) //above
+				{
+					var height = (100-data1)/(100-min)*(315-axis_location);
+					processing.rect(horz_locs[i-1]-16, axis_location - 15, 16, height);
+					above_1[i-1] = axis_location - 15 + height;
+				}
+				else if (data1 < 100)
+				{
+					var height = (100-data1)/(100-min)*(315-axis_location);
+					processing.rect(horz_locs[i-1]-16, axis_location + 15, 16, height);
+					below_1[i-1] = axis_location + 15 + height;
+				}
+				else
+				{
+					processing.rect(horz_locs[i-1], axis_location - 15, 16, -5);
+					above_1[i-1] = axis_location-25;
+				}
+			}
 
+			if (!hide_2)
+			{
+				var data2 = data["cli_" + i + "_2"];
+				processing.fill(gray);
+				if (data2 > 100) //above
+				{
+					var height = (100-data2)/(100-min)*(315-axis_location);
+					processing.rect(horz_locs[i-1], axis_location - 15, 16, height);
+					above_2[i-1] = axis_location - 15 + height;
+				}
+				else if (data2 < 100)
+				{
+					var height = (100-data2)/(100-min)*(315-axis_location);
+					processing.rect(horz_locs[i-1], axis_location + 15, 16, height);
+					below_2[i-1] = axis_location + 15 + height;
+				}
+				else
+				{
+					processing.rect(horz_locs[i-1], axis_location - 15, 16, -5);
+					above_2[i-1] = axis_location-25;
+				}
+			}
+
+		}
+
+
+		processing.stroke(0);
+		processing.line(60, axis_location - 15, 605, axis_location - 15);
+		processing.line(60, axis_location + 15, 605, axis_location + 15);
+
+		//title and help
+		processing.line(185, 30, 487, 30);
+		processing.line(213, 347, 459, 347);
+		processing.fill(0);
+		processing.text("OVERALL AND CATEGORICAL COST OF LIVING (%)", 335, 25);
+		
+		processing.text("MOUSE OVER DATA FOR MORE DETAILS", 337, 362);
+		
 	};
+
 
 	function labor_stats() {
 		//draw axis
@@ -205,6 +485,8 @@ function sketchProc(processing) {
 			processing.rect(493, 299 - 200 * (data["econgrow_2"]/max_econ), 42, 200 * (data["econgrow_2"]/max_econ));
 		}
 
+		
+
 	};
 
 	function taxes() {
@@ -241,7 +523,7 @@ function sketchProc(processing) {
 		}
 
 		//background separation
-		processing.fill(250);
+		processing.fill(245);
 		processing.noStroke();
 		processing.rect(100, 55, 48, 268);
 		processing.rect(190, 55, 48, 268);
