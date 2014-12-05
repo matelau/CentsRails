3374aba6ed4e029781d1e06748e3e623f76de896
oakfield <michael.christian.smith@gmail.com>
2014-12-04 23:45:10 -0700

    api now does basic error checking and accepts json

--- app/controllers/api/v1/coli_controller.rb
+++ app/controllers/api/v1/coli_controller.rb
@@ -10,10 +10,34 @@ class Api::V1::ColiController < ApplicationController
 
 	# Retrieve all location data by location name.
 	# (Well, it SHOULD. Right now it looks up some location data by ID.)
-	# TODO:	(1) Accept JSON data using the correct protocol.
 	# 			(2) Retrieve weather data also.
-	#				(3) Error checking, including reasonable status codes for bad input.
 	def show
-		render json: Coli.where(location: params[:location]), status: 200
+		# Check for the required fields, and return an appropriate message if
+		# they are not present.
+		unless params[:search_by]
+			message = {'error' => 
+				"The search_by field was empty. It should be a column name."}
+			render json: message, status: 400
+			return
+		end
+
+		unless params[:operation]
+			message = {'error' => 
+				"The operation field was empty. It should be something like 'get.'"}
+			render json: message, status: 400
+			return
+		end
+	
+		unless params[:objects]
+			message = {'error' => 
+				"The objects field was empty. It should be an array."}
+			render json: message, status: 400
+			return
+		end
+
+#		render json: Coli.where(location: params['search_by']), status: 200
+		render json: Coli.where(["? = ?", 
+														params[:search_by], 
+														params[:objects][0]["base city"]]) # TODO: iterate
 	end
 end
