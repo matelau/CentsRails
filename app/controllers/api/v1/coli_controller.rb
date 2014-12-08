b68e830437365385016e11dceebe06ef51641955
oakfield <michael.christian.smith@gmail.com>
2014-12-07 21:38:27 -0700

    api now returns COLI data, also I fixed a copy/paste error in the labor max/min data

--- app/controllers/api/v1/coli_controller.rb
+++ app/controllers/api/v1/coli_controller.rb
@@ -41,6 +41,8 @@ class Api::V1::ColiController < ApplicationController
 		#end
 
 		# Add data for each object.
+		# We'll need to both iterate over each location and keep track of their
+		# indices, because that's how the view tracks them.
 		i = 1
 		objects.each do |object|
 			location = object.values[0]
@@ -48,24 +50,67 @@ class Api::V1::ColiController < ApplicationController
 			# Name each location.
 			result["location_#{i}".to_sym] = location
 	
+		# Large array of:
+		# cli_[city]_[category] 
+		# (overall, city, goods, groceries, healthcare, housing, trans, utilities)
+		# Followed by:
+		# cli_max
+		# cli_min
+			##### ---------------- COST OF LIVING ---------------- #####
+			# Get overall COLI data for this location.
+			records = Coli.select(:cost_of_living)
+										.where(['location = ?', location])
+			result["cli_#{i}_1"] = records[0][:cost_of_living]
+
+			# Re-send the location for the visualization.
+			result["cli_#{i}_2"] = location
+
+			# Get goods COLI data for this location.
+			records = Coli.select(:goods)
+										.where(['location = ?', location])
+			result["cli_#{i}_3"] = records[0][:goods]
+
+			# Get groceries COLI data for this location.
+			records = Coli.select(:groceries)
+										.where(['location = ?', location])
+			result["cli_#{i}_4"] = records[0][:groceries]
+
+			# Get healthcare COLI data for this location.
+			records = Coli.select(:health_care)
+										.where(['location = ?', location])
+			result["cli_#{i}_5"] = records[0][:health_care]
+
+			# Get housing COLI data for this location.
+			records = Coli.select(:housing)
+										.where(['location = ?', location])
+			result["cli_#{i}_6"] = records[0][:housing]
+
+			# Get transportation COLI data for this location.
+			records = Coli.select(:transportation)
+										.where(['location = ?', location])
+			result["cli_#{i}_7"] = records[0][:transportation]
+
+			# Get utilities COLI data for this location.
+			records = Coli.select(:utilities)
+										.where(['location = ?', location])
+			result["cli_#{i}_8"] = records[0][:utilities]
+			#### ---------------- END COST OF LIVING ------------- ####
 	
+
 			##### -------------------- LABOR --------------------- #####
 			# Get unemployment data for this location.
 			records = Coli.select(:unemp_rate)
 										.where(['location = ?', location])
-			
 			result["labor_#{i}_1"] = records[0][:unemp_rate]
 
 			# Get income data for this location.
 			records = Coli.select(:income)
 										.where(['location = ?', location])
-			
 			result["labor_#{i}_2"] = records[0][:income]
 
 			# Get growth data for this location.
 			records = Coli.select(:unemp_trend)
 										.where(['location = ?', location])
-			
 			result["labor_#{i}_1"] = records[0][:unemp_trend]
 			##### -------------------- END LABOR ----------------- #####
 
@@ -99,36 +144,33 @@ class Api::V1::ColiController < ApplicationController
 			i += 1
 		end
 
+		# Get max and min of overall COLI data.
+		result["cli_max"] = Coli.maximum(:cost_of_living)
+		result["cli_min"] = Coli.minimum(:cost_of_living)	
+
 		# Get max and min of unemployment data.
 		result["labor_max_1"] = Coli.maximum(:unemp_rate)
 		result["labor_min_1"] = Coli.minimum(:unemp_rate)
 		
 		# Get max and min of income data.
-		result["labor_max_2"] = Coli.maximum(:unemp_rate)
-		result["labor_min_2"] = Coli.minimum(:unemp_rate)
+		result["labor_max_2"] = Coli.maximum(:income)
+		result["labor_min_2"] = Coli.minimum(:income)
 
 		# Get max and min of growth data.
-		result["labor_max_3"] = Coli.maximum(:unemp_rate)
-		result["labor_min_3"] = Coli.minimum(:unemp_rate)
+		result["labor_max_3"] = Coli.maximum(:unemp_trend)
+		result["labor_min_3"] = Coli.minimum(:unemp_trend)
 
 		# Send min and max weather values.
 		result["max_weather"] = WeatherRecord.maximum(:high)
 		result["min_weather"] = WeatherRecord.minimum(:low)
 	
 		# Large array of:
-		# cli_[city]_[category] 
-		# (overall, city, goods, groceries, healthcare, housing, trans, utilities)
-		# Followed by:
-		# cli_max
-		# cli_min
-
-		# Large array of:
 		# taxes_city_[1, 2, 3]
 		# Followed by:
 		# taxes_max
 		# taxes_min
 
-		# Return the result.
+		# Return the result, formatted as JSON, and with a 200 OK HTTP code.
 		render json: result, status: 200
 		# End querying the database.
 	end
