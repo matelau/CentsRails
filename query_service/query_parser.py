485fae15238869138e180c9579a4aceda95207dd
Austin Hammer <hammer.aj@gmail.com>
2014-12-11 22:37:00 -0700

    changed ip address

--- query_service/query_parser.py
+++ query_service/query_parser.py
@@ -138,7 +138,7 @@ def query(query):
 		}
 		for l in locations:
 			package["objects"].append({"city":l[:l.index(",")]})
-		url = "/api/v1/coli/"
+		url = "http://54.183.8.236/api/v1/coli/"
 		payload = json.dumps(package)
 		r = requests.Request("POST",url,headers={'Content-Type':'application/json','Accept':'application/json'},data=payload)
 		prep = r.prepare()
