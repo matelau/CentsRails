8ef7cf3eb017541d614ca8be317cd88e88d6ad87
Austin Hammer <hammer.aj@gmail.com>
2015-02-11 16:47:33 -0700

    parser multithread

--- query_service/query_parser.py
+++ query_service/query_parser.py
@@ -230,4 +230,4 @@ def query(query):
 	return resp
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0',port=6001,debug=True,ssl_context=('/etc/ssl/certs/ssl-bundle.crt','../.ssl/myserver.key'))
+	app.run(host='0.0.0.0',port=6001,debug=True,processes=5,ssl_context=('/etc/ssl/certs/ssl-bundle.crt','../.ssl/myserver.key'))
