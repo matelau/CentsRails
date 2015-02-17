358fe177bcff1866d958920bf9aeee8f42fb0a85
Wesley Gormley <wesleypalmer83@gmail.com>
2015-02-17 14:55:57 -0700

    saving changes before switching branches

--- app/assets/javascripts/examples.js
+++ app/assets/javascripts/examples.js
@@ -2,7 +2,7 @@
 
 function showSearch(type, side) {
 	$("#" + type + "_container").empty();
-	var toAdd = "<div id='" + type + "_search' height='300px' width='540px' style='display:inline; float:left; background-image: url(/assets/" + type + "_compare_blur.jpeg); height: 300px; width: 540px; margin-" + side + ": 10px'></div></li>";
+	var toAdd = "<div id='" + type + "_search' height='300px' width='540px' style='display:inline; float:left; box-shadow: -5px 5px 3px #CDCDCD; background-image: url(/assets/" + type + "_compare_blur.jpeg); height: 300px; width: 540px; margin-" + side + ": 10px'></div></li>";
 	$(toAdd).appendTo("#" + type + "_container");
 	//build search objects
 	var search = "<form><ul class='examples_search'><li><input class='search_1_examples' id='search_1_name' type='text'></li>";
@@ -16,7 +16,7 @@ function showSearch(type, side) {
 
 function resort(type, side) {
 	$("#" + type + "_container").empty();
-	var toAdd = "<div height='300px' width='540px' onclick='showSearch(&quot;" + type + "&quot;, &quot;" + side + "&quot;)' style='display:inline; float:left;";
+	var toAdd = "<div height='300px' width='540px' onclick='showSearch(&quot;" + type + "&quot;, &quot;" + side + "&quot;)' style='display:inline; box-shadow: -5px 5px 3px #CDCDCD; float:left;";
 	toAdd += "background-image: url(/assets/" + type + "_compare.jpeg); height: 300px; width: 540px; margin-" + side + ": 10px'></div>";
 	$(toAdd).appendTo("#" + type + "_container");
 };
