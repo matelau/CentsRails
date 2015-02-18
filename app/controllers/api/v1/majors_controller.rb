a5e6115d03bc6d615827c6b5f100a1bd73ac86ff
oakfield <michael.christian.smith@gmail.com>
2015-02-18 16:16:23 -0700

    Basic majors controller working. TODO: test with viz

--- app/controllers/api/v1/majors_controller.rb
+++ app/controllers/api/v1/majors_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::MajorsController < ApplicationController
+
+	def show
 		result = Hash.new
 		error_list = []
 
@@ -19,24 +21,24 @@ class Api::V1::MajorsController < ApplicationController
 
 		majors = params[:majors]
 
-		# Create a string of the form 'name = ? OR ... ' and a list of major names.
+		# Create a string of the form 'd.name = ? OR ... ' and a list of major names.
 		# This string and list will be used to dynamically create the where clause
 		# for the query.
 		where_string = ""
 		where_params = Array.new
 		majors.each do |major|
-			where_string += 'name = ? OR '
+			where_string += 'd.name = ? OR '
 			where_params << major[:name]
 		end
 		where_string = where_string[0..-5]	# Strip off the last ' OR '.
 
-		records = Degrees.find_by_sql [
-			"SELECT d.name,
-							d.salary,
+		records = Degree.find_by_sql [
+			"SELECT d.name AS degree_name,
+							d.salary AS degree_salary,
 							d.recommend,
 							d.satisfaction,
-							j.name,
-							j.salary
+							j.name AS job_name,
+							j.salary AS job_salary
 			FROM degrees AS d
 			LEFT OUTER JOIN top_jobs AS j
 			ON d.id = j.degree_id
@@ -50,18 +52,24 @@ class Api::V1::MajorsController < ApplicationController
 		# (The index is needed because that's how the view tracks majors.)
 		index = 1
 		majors.each do |major|
+			result["jobs_#{index}"] = Array.new
 			match = false
 
 			# Search through the retrieved records for an exact match.
 			records.each do |record|
-				if record[:name]  == major[:name]
+				if record[:degree_name]  == major[:name]
 					match = true
-					salary = record[:salary] ? record[:salary].to_f : nil
+
+					salary = record[:degree_salary] ? record[:degree_salary].to_f : nil
 					recommended = record[:recommend] ? record[:recommend].to_f : nil
 					satisfaction = record[:satisfaction] ? record[:satisfaction].to_f : nil
+					job_name = record[:job_name] ? record[:job_name] : nil
+					job_salary = record[:job_salary] ? record[:job_salary].to_f : nil
 					# Cents rating goes here
 					cents_rating = 0
+
 					result["major_#{index}"] = [salary, recommended, satisfaction, cents_rating]
+					result["jobs_#{index}"].concat [job_name, job_salary]
 					break
 				end
 			end
