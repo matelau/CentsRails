10a2f63ad5b2ba16fb19b5d3142ccc50ce2d0eef
Austin Hammer <hammer.aj@gmail.com>
2015-02-22 20:25:38 -0700

    initial async request

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,7 +32,6 @@ if (path[1] == "wizard" && path[2] != "start" && path[2] != "education")
 else if (path[1] == "search" && path[2] == "results")
 {
 	var query_type = localStorage.getItem("query_type");
- 	//localStorage.removeItem("query_type");
 
 	getPartial(query_type);
 }
@@ -40,66 +39,43 @@ else if (path[1] == "search" && path[2] == "results")
 
 function getPartial(query_type){
 	$.get("../getPartial", {query_type: query_type}, function(response){  
-	$(".center").remove();               
-  	$(response).appendTo("#main_body");
-  	var script = document.createElement("script");
-	script.type = "application/javascript";
-	script.src = "/../assets/" + query_type + ".js";
-	document.getElementsByTagName("body")[0].appendChild(script);
-  });
+		$(".center").remove();               
+	  	$(response).appendTo("#main_body");
+	  	var script = document.createElement("script");
+		script.type = "application/javascript";
+		script.src = "/../assets/" + query_type + ".js";
+		document.getElementsByTagName("body")[0].appendChild(script);
+	});
 
 };
 
-
 function api_request(query) {
-
-	//send request to api
-
-	//data returned
 	var data =  new Object();
 
 	var url = "https://www.trycents.com:6001/query/" + query;
 
-	var xmlHttp = null;
+	var xhr = null;
 
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", url, false );
-    xmlHttp.send( null );
-    data = jQuery.parseJSON(xmlHttp.responseText);
+    xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
 
-	//console.log(data)
+    xhr.onreadystatechange = function() {
+    	if (xhr.readyState === 4) { 
+      		if (xhr.status === 200) {
+      			data = jQuery.parseJSON(xhr.responseText);
 
-	//store query type in local storage
-
-	/*data.weather_1 =    [38.0, 44.0, 53.0, 61.0, 71.0, 82.0, 90.0, 89.0, 78.0, 65.0, 50.0, 40.0, 38.0, 90.0];
-	data.weatherlow_1 = [26.0, 31.0, 38.0, 43.0, 52.0, 61.0, 69.0, 67.0, 58.0, 46.0, 36.0, 27.0, 26.0, 69.0];
-	data.weather_2    = [67.0, 71.0, 77.0, 85.0, 95.0, 104.0, 106.0, 104.0, 100.0, 89.0, 76.0, 66.0, 66.0, 106.0];
-	data.weatherlow_2 = [46.0, 49.0, 53.0, 60.0, 69.0, 78.0, 83.0, 83.0, 77.0, 65.0, 53.0, 45.0, 45.0, 83.0];
-
-	data.location_1 = "Salt Lake City, UT";
-	data.location_2 = "Phoenix, AZ";
-
-	data.cli_1 = [102, 94, 95, 95, 119, 105, 92, 92, 119];
-	data.cli_2 = [96, 92, 100, 106, 97, 101, 99, 92, 106];
-
-	data.labor_1 = [3.4, 48000, 4.2];
-	data.labor_2 = [6.4, 51000, null];
-	data.labor_3 = [5.8, 44800, 4.6];
-
-	data.taxes_1 = [6.85, 5.0, 5.0, 0.67];
-	data.taxes_2 = [8.3, 2.59, 4.54, 1.59];
-	data.taxes_3 = [8.25, 3.5, 7.8, 1.15];*/
-
-	if(data["operation"] == "undefined") {
-		window.location = "/info/examples/";
-	}
-	else {
-		//write data to local storage for results page
-		localStorage.removeItem("data_store");
-		localStorage.setItem("data_store", JSON.stringify(data));
-		localStorage.setItem("query_type", data["query_type"]);
-
-		window.location = "/search/results/";
-	}
+				if(data["operation"] == "undefined") {
+					window.location = "/info/examples/";
+				}
+				else {
+    				localStorage.removeItem("data_store");
+					localStorage.setItem("data_store", JSON.stringify(data));
+					localStorage.setItem("query_type", data["query_type"]);
+				}
+      		}
+  		}
+  	}
+  	window.location = "/search/results/";
+  	xhr.send(null);
 };
 
