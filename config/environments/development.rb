fccb6268833d8af639195543b1da1698850ff0e5
oakfield <michael.christian.smith@gmail.com>
2015-03-05 20:43:49 -0700

    Changed mail port to 25

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
   	address: "smtp.gmail.com",
-  	port: "587",
+  	port: "25",
   	domain: "gmail.com",
   	user_name: "michael.christian.smith@gmail.com",
   	password: "He%wasimmense1google",
