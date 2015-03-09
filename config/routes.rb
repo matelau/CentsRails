3d0912d5bf99d401ac63264d7e9cccef52ef43ef
oakfield <michael.christian.smith@gmail.com>
2015-03-08 21:13:24 -0600

    Added terms and conditions page.

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   get 'user/logout' => 'sessions#destroy'
   get 'search/results'
   get 'search/getPartial'
+  get 'user/terms'
   root 'search#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
