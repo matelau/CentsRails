769a03c6058e7cfb4225b3c5cccbcc93aef91a7a
oakfield <michael.christian.smith@gmail.com>
2015-03-09 15:18:16 -0600

    Previous change didn't work; trying again

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,15 @@ class SessionsController < ApplicationController
 
 	# Log in the user by creating a session.
 	def create
-		@user = User.find_by_email(params[:email])
+		user = User.find_by_email(params[:email])
 
-		Rails.logger = Logger.new(STDOUT)
+		Rails.logger = Logger.new("login.log")
 		Rails.logger.level = 0
-		logger.debug "user: #{@user.attributes.inspect}"
+		logger.debug "user: #{user.attributes.inspect}"
 
 		# Check that the user passes the model's tests.
-		if @user && @user.authenticate(params[:password])
-			logger.debug "user: #{@user.attributes.inspect}"
+		if user && user.authenticate(params[:password])
+			logger.debug "user: #{user.attributes.inspect}"
 
 			# Check that the user is in the MailChimp list.
 			# cents_members is a hash with 'total' and 'data' fields.
@@ -22,12 +22,12 @@ class SessionsController < ApplicationController
 			in_list = false
 			cents_members = @@mailchimp.lists.members(@@mc_list_id)
 			cents_members['data'].each do |member|
-				if member['email'] == @user.email
+				if member['email'] == user.email
 					in_list = true
 				end
 			end
 			if in_list
-				session[:user_id] = @user.id
+				session[:user_id] = user.id
 				redirect_to '/'
 			end
 		else
