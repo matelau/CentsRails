caff886f31e17751d493d2cfe7c3f1180d14183d
Wesley Gormley <wesleypalmer83@gmail.com>
2015-04-01 22:56:16 -0600

    home page slider, with search bar auto complete

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,6 +2,12 @@ var slider;
 
 $(document).ready(function() {
 	slider = new IdealImageSlider.Slider('#examples_slider');
+	//set all alt tags
+	document.getElementById("career").setAttribute("data-caption", "CLICK FOR A POPULAR CAREER COMPARISON");
+	document.getElementById("city").setAttribute("data-caption", "CLICK FOR A POPULAR CITY COMPARISON");
+	document.getElementById("spend").setAttribute("data-caption", "CLICK FOR A POPULAR SPENDING BREAKDOWN");
+	document.getElementById("major").setAttribute("data-caption", "CLICK FOR A POPULAR MAJOR COMPARISON");
+	document.getElementById("school").setAttribute("data-caption", "CLICK FOR A POPULAR SCHOOL COMPARISON");
 	slider.addCaptions();
     slider.start();
 });
@@ -21,5 +27,14 @@ function startSlider() {
 function popular_populate(type) {
 	//console.log(document.getElementById('#examples_slider').src);
 	//console.log(type);
-	document.getElementById("search").value = type;
+	if (type == "city")
+		document.getElementById("search").value = "Dallas, TX vs Madison, WI";
+	if (type == "major")
+		document.getElementById("search").value = "Animal Science vs Civil Engineering";
+	if (type == "school")
+		document.getElementById("search").value = "Stanford vs Bama";
+	if (type == "career")
+		document.getElementById("search").value = "future career seach";
+	if (type == "spend")
+		document.getElementById("search").value = "What can I afford with my salary?";
 };
