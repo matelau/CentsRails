00394e66e445d0a3d5313743555cdc82b443627f
Austin Hammer <hammer.aj@gmail.com>
2015-04-12 21:17:17 -0600

    testing calls

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -76,7 +76,7 @@ function api_request(query) {
 				else {
     				localStorage.removeItem("data_store");
     				res = JSON.stringify(data);
-    				console.log(data["locations"].length())
+    				console.log(data["locations"])
 					localStorage.setItem("data_store", JSON.stringify(data));
 					localStorage.setItem("query_type", data["query_type"]);
 					window.location = "/search/results/";
