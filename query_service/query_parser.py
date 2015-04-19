513e2ad1f78e3eec67a97cf7d1bdb360b8784a8b
Austin Hammer <hammer.aj@gmail.com>
2015-04-19 14:40:55 -0600

    testing fix

--- query_service/query_parser.py
+++ query_service/query_parser.py
@@ -153,7 +153,7 @@ def query(sent_query):
 			careers.append(ordered_keys[idx][0])
 			m = cgrams[ordered_keys[idx][0]]
 			idx += 1
-			if idx > len(ordered_keys):
+			if idx >= len(ordered_keys):
 				break
 		#if(c.lower() in query):
 		#	careers.append(c)
