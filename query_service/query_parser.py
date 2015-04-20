8322cf7c1c0af75552b5c82414197715783e140d
Austin Hammer <hammer.aj@gmail.com>
2015-04-20 00:29:57 -0600

    test random

--- query_service/query_parser.py
+++ query_service/query_parser.py
@@ -105,7 +105,7 @@ def query(sent_query):
 			sfault = True
 
 	if sval == "random" and dval == "":
-		value = os.urandom(1)[0]
+		value = struct.unpack("<L", os.urandom(1))
 		print value
 		print random.uniform(0,0.3)
 		idx = hp.get_rand(0,3)
